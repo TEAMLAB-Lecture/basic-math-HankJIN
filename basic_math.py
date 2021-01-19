ca6279313513b65779c0286b56e426cb83dcb54e
HankJIN <djrjtk@gmail.com>
2021-01-19 15:16:50 +0900

    update my code

--- basic_math.py
+++ basic_math.py
@@ -8,44 +8,14 @@
 
 
 def get_greatest(number_list):
-    """
-    주어진 리스트에서 가장 큰 숫자를 반환함
-
-        Parameters:
-            number_list (list): integer로 값으로만 구성된 list
-            ex - [10, 33, 22, 99, 33]
-
-        Returns:
-            greatest_number (int): parameter number_list 중 가장 큰 값
-
-        Examples:
-            >>> number_list = [39, 54, 32, 11, 99]
-            >>> import basic_math as bm
-            >>> bm.get_greatest(number_list)
-            99
-    """
-    greatest_number = None
+    number_list.sort()
+    greatest_number = number_list[-1]
     return greatest_number
 
 
 def get_smallest(number_list):
-    """
-    주어진 리스트에서 제일 작은 숫자를 반환함
-
-        Parameters:
-            number_list (list): integer로 값으로만 구성된 list
-            ex - [10, 33, 22, 99, 33]
-
-        Returns:
-            smallest_number (int): parameter number_list 중 가장 작은 값
-
-        Examples:
-            >>> number_list = [39, 54, 32, 11, 99]
-            >>> import basic_math as bm
-            >>> bm.get_smallest(number_list)
-            11
-    """
-    smallest_number = None
+    number_list.sort()
+    smallest_number = number_list[0]
     return smallest_number
 
 
@@ -66,7 +36,10 @@ def get_mean(number_list):
             >>> bm.get_mean(number_list)
             47
     """
-    mean = None
+    cnt = len(number_list)
+    for item in number_list :
+        sum += item
+    mean = sum/len
     return mean
 
 
@@ -90,5 +63,11 @@ def get_median(number_list):
             >>> bm.get_median(number_list2)
             35.5
     """
-    median = None
+    cnt = len(number_list)
+    number_list.sort()
+
+    if(cnt % 2 == 1):
+        median = number_list[cnt/2]
+    else:
+        median = (number_list[cnt/2 - 1] + number_list[cnt/2]) /2
     return median
