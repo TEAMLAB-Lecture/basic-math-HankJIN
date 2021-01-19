d16bda961f8b82d3b0326332aee0cdffa79af0d6
HankJIN <djrjtk@gmail.com>
2021-01-19 16:53:21 +0900

    update code

--- basic_math.py
+++ basic_math.py
@@ -20,22 +20,6 @@ def get_smallest(number_list):
 
 
 def get_mean(number_list):
-    """
-    주어진 리스트 숫자들의 평균을 구함.
-
-        Parameters:
-            number_list (list): integer로 값으로만 구성된 list
-            ex - [10, 33, 22, 99, 33]
-
-        Returns:
-            mean (int): parameter number_list 숫자들의 평균
-
-        Examples:
-            >>> number_list = [39, 54, 32, 11, 99]
-            >>> import basic_math as bm
-            >>> bm.get_mean(number_list)
-            47
-    """
     cnt = len(number_list)
     for item in number_list :
         sum += item
@@ -44,30 +28,10 @@ def get_mean(number_list):
 
 
 def get_median(number_list):
-    """
-    주어진 리스트 숫자들의 중간값을 구함.
-
-        Parameters:
-            number_list (list): integer로 값으로만 구성된 list
-            ex - [10, 33, 22, 99, 33]
-
-        Returns:
-            median (int): parameter number_list 숫자들의 중간값
-
-        Examples:
-            >>> number_list = [39, 54, 32, 11, 99]
-            >>> import basic_math as bm
-            >>> bm.get_median(number_list)
-            39
-            >>> number_list2 = [39, 54, 32, 11, 99, 5]
-            >>> bm.get_median(number_list2)
-            35.5
-    """
     cnt = len(number_list)
     number_list.sort()
-
-    if(cnt % 2 == 1):
-        median = number_list[cnt/2]
-    else:
-        median = (number_list[cnt/2 - 1] + number_list[cnt/2]) /2
+    if cnt%2==1:
+        median = number_list[cnt//2]
+    else :
+        median = (number_list[cnt//2-1] + number_list[cnt//2])/2
     return median
